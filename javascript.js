d237a9a51b69313db2eabe31d1c85d9da4daf07e
kayaba <Stefan.siggaard.andersen@gmail.com>
2022-11-23 11:07:58 +0100

    Wrote play round algorithm

--- javascript.js
+++ javascript.js
@@ -19,3 +19,27 @@ function getComputerChoice() {
             break;
     }
 }
+
+// Test of computer choice
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+
+// Returns the winner of the game
+function playRound(playerChoice, ComputerChoice){
+    if (playerChoice === ComputerChoice){
+        return "TIE! No one";
+    } else if (playerChoice === "rock" && ComputerChoice === "scissors"){
+        return "Player";
+    } else if (playerChoice === "paper" && ComputerChoice === "rock"){
+        return "Player";
+    } else if (playerChoice === "scissors" && ComputerChoice === "paper"){
+        return "Player";
+    } else {
+        return "Computer";
+    }
+}
+
+console.log(playRound("rock", getComputerChoice()));
