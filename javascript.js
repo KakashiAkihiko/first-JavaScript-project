15687ae690bde62e48ea670f1777df9e18c2cfac
kayaba <Stefan.siggaard.andersen@gmail.com>
2022-11-23 13:47:30 +0100

    Added playround function and loops 5 rounds

--- javascript.js
+++ javascript.js
@@ -3,7 +3,7 @@
 function getComputerChoice() {
     min = Math.ceil(1);
     max = Math.floor(3);
-    switch (Math.floor(Math.random() * (max - min + 1)) + min){
+    switch (Math.floor(Math.random() * (max - min + 1)) + min) {
         case 1:
             return "rock";
             break;
@@ -13,7 +13,7 @@ function getComputerChoice() {
         case 3:
             return "scissors";
             break;
-        
+
         default:
             return "spock";
             break;
@@ -21,25 +21,62 @@ function getComputerChoice() {
 }
 
 // Test of computer choice
-console.log(getComputerChoice());
-console.log(getComputerChoice());
-console.log(getComputerChoice());
-console.log(getComputerChoice());
-console.log(getComputerChoice());
+console.log("Computer Choice test: " + getComputerChoice());
+console.log("Computer Choice test: " + getComputerChoice());
+console.log("Computer Choice test: " + getComputerChoice());
+console.log("Computer Choice test: " + getComputerChoice());
+console.log("Computer Choice test: " + getComputerChoice());
 
 // Returns the winner of the game
-function playRound(playerChoice, ComputerChoice){
-    if (playerChoice === ComputerChoice){
+function playRound(playerChoice, ComputerChoice) {
+    if (playerChoice === ComputerChoice) {
         return "TIE! No one";
-    } else if (playerChoice === "rock" && ComputerChoice === "scissors"){
+    } else if (playerChoice === "rock" && ComputerChoice === "scissors") {
         return "Player";
-    } else if (playerChoice === "paper" && ComputerChoice === "rock"){
+    } else if (playerChoice === "paper" && ComputerChoice === "rock") {
         return "Player";
-    } else if (playerChoice === "scissors" && ComputerChoice === "paper"){
+    } else if (playerChoice === "scissors" && ComputerChoice === "paper") {
         return "Player";
     } else {
         return "Computer";
     }
 }
 
-console.log(playRound("rock", getComputerChoice()));
+
+
+function game() {
+    let playerChoice = "none";
+
+    //Prompt the player for inputer and put it to lower case
+    playerChoice = (prompt("Please enter \"Rock, Paper or Scissors\"")).toLowerCase();
+
+    //Check if user input is correct, otherwise prompt again
+    while(playerChoice !== "rock" &&
+    playerChoice !== "paper" &&
+    playerChoice !== "scissors"){
+        playerChoice = (prompt("You suck... Enter: \"Rock, Paper or Scissors\"")).toLowerCase();
+    }
+
+    switch (playRound(playerChoice, getComputerChoice())){
+        case "Player":
+            playerWins++;
+            console.log("The Player wins!");
+            break;
+        case "Computer":
+            computerWins++;
+            console.log("The Computer wins!");
+            break;
+        case "TIE! No one":
+            console.log("Tie! No one wins");
+    }
+
+}
+
+//Play 5 games
+let playerWins = 0;
+let computerWins = 0;
+
+for(let i=0; i<5; i++){
+    game();
+    console.log("The current score is: Player " + playerWins + " || Computer " + computerWins);
+}
